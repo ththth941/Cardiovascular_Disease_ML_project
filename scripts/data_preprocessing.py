28f71e0160fcfc7a96d299bec862f8faf42d516f
ththth941 <thuanlactic123@gmail.com>
2024-11-25 17:59:11 +0700

    Update data_preprocessing.py

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,27 +1,26 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import pickle
 
 def load_data(file_path):
-    """
-    Đọc dữ liệu từ file CSV.
-    """
-    df = pd.read_csv('data\\Cardiovascular_Disease_Dataset.csv')
+    df = pd.read_csv(file_path)
     return df
 
 def preprocess_data(df):
-    """
-    Tiền xử lý dữ liệu: tách biến đầu vào (X) và nhãn (y), chia dữ liệu train/test, chuẩn hóa dữ liệu.
-    """
-    X = df.drop(columns=['patientid', 'target'])
-    y = df['target']
+    X = df.drop(["patientid", "target"], axis=1)
+    y = df["target"]
     
-    # Chia tập dữ liệu thành train và test
+    # Split into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    # Chuẩn hóa dữ liệu
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    # Scaling the data
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
     
-    return X_train_scaled, X_test_scaled, y_train, y_test, scaler
+    mean_std_values = {'mean': scaler.mean_, 'std': scaler.scale_}
+    with open('models/mean_std_values_ML.pkl', 'wb') as f:
+        pickle.dump(mean_std_values, f)
+    
+    return X_train, X_test, y_train, y_test, scaler
