d849ecc62123669faea3858f24620cf622867d0d
ththth941 <thuanlactic123@gmail.com>
2024-11-25 18:00:17 +0700

    Update evaluate_model.py

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,34 +1,18 @@
-import pickle
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import cross_val_score
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report, cross_val_score
 
-def evaluate_accuracy(model, X_test_scaled, y_test):
-    """
-    Đánh giá độ chính xác của mô hình.
-    """
-    accuracy = model.score(X_test_scaled, y_test)
-    return accuracy
+def plot_conf_matrix(y_test, y_preds):
+    fig, ax = plt.subplots(figsize=(3, 3))
+    ax = sns.heatmap(confusion_matrix(y_test, y_preds), annot=True, cbar=False)
+    plt.xlabel("True Label")
+    plt.ylabel("Predicted Label")
 
-def evaluate_confusion_matrix(model, X_test_scaled, y_test):
-    """
-    Đánh giá mô hình bằng ma trận nhầm lẫn.
-    """
-    y_pred = model.predict(X_test_scaled)
-    cm = confusion_matrix(y_test, y_pred)
-    cr = classification_report(y_test, y_pred)
-    return cm, cr
+def evaluate_model(model, X_test, y_test):
+    y_preds = model.predict(X_test)
+    plot_conf_matrix(y_test, y_preds)
+    print(f"{model.__class__.__name__} Classification Report:")
+    print(classification_report(y_test, y_preds))
 
-def evaluate_cross_validation(model, X_scaled, y):
-    """
-    Đánh giá mô hình bằng cross-validation.
-    """
-    cv_scores = cross_val_score(model, X_scaled, y, cv=5)
-    return cv_scores
-
-def load_model(model_name):
-    """
-    Tải mô hình đã lưu từ file pickle.
-    """
-    with open(model_name, 'rb') as file:
-        model = pickle.load(file)
-    return model
+def cross_validate_model(model, X, y):
+    cv_acc = cross_val_score(model, X, y, cv=5, scoring="accuracy")
+    return cv_acc.mean()
