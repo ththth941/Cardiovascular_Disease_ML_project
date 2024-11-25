824206a157f758b4a8e1d16e9675870430394e12
ththth941 <thuanlactic123@gmail.com>
2024-11-25 17:21:34 +0700

    Update model_train.py

--- src/model_traning/model_train.py
+++ src/model_traning/model_train.py
@@ -1,45 +1,41 @@
 
-# Regular EDA (Exploratory data analysis) and ploting libraries
+# Import các thư viện
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
-# Models from Scikit-learn
+# mô hình từ thư viện scikitlearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-# Model Evaluation
+# thư viện đánh giá mô hình
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import confusion_matrix, classification_report
 
-# %%
-df = pd.read_csv("E:\\Cardiovascular_Disease_Dataset.csv")
+df = pd.read_csv("src\\data\\Cardiovascular_Disease_Dataset.csv")
 print(df.head(10))
 
 nRow, nCol = df.shape
 print("Shape of dataset {}".format(df.shape))
 print(f"Rows: {nRow} \nColumns: {nCol}")
 
-
 print(df.columns)
 
-# %%
 # Split data into X and y
 X = df.drop(["patientid", "target"], axis=1)
 
 y = df["target"]
 
-# %%
-# Split data into train and test sets
+# Chia data thành tập train và tập test
 np.random.seed(42)
 
 # Split into train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Scaling the data
+# Scaling data
 
 from sklearn.preprocessing import StandardScaler
 
@@ -48,20 +44,18 @@ scaler = StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-# %%
 mean_std_values = {'mean': scaler.mean_, 'std': scaler.scale_}
 with open('E:\\Python Projects\\Heart-Disease_Project\\model\\mean_std_values_ML_2.pkl', 'wb') as f:
     pickle.dump(mean_std_values, f)
-
-# %%
-# Puts model into a dictionary
+    
+# Đưa các model vào dictionary
 
 models = {"Logistic Regression": LogisticRegression(),
           "KNN": KNeighborsClassifier(),
           "Random Forest": RandomForestClassifier()}
 
 
-# Create a function to fit and score models
+# Tạo hàm để fit và chấm điểm model
 
 def fit_and_score(models, X_train, X_test, y_train, y_test):
     # Set random seed
@@ -69,73 +63,47 @@ def fit_and_score(models, X_train, X_test, y_train, y_test):
     # Make a dictionary to keep model scores
     model_scores = {}
     for name, model in models.items():
-        # Fit the model to data
+        # Fit model vào data
         model.fit(X_train, y_train)
-        # Evaluate the model and append its score to model_scores
+        # Chấm điểm model
         model_scores[name] = model.score(X_test, y_test)
     return model_scores
 
-
-# %%
 model_scores = fit_and_score(models, X_train, X_test, y_train, y_test)
 print(model_scores)
 
-# %% [markdown]
-# ### Model Comparison
+# So Sánh Model 
 
-# %%
-model_compare = pd.DataFrame(model_scores,
-                             index=["Accuracy"])
+model_compare = pd.DataFrame(model_scores, index=["Accuracy"])
 model_compare.T.plot.bar()
 plt.xticks(rotation=0);
 
-# %% [markdown]
-# Now we've got a baseline model.. and we know a model's first prediction aren't always what we should based our next setp off. what should we do?
-#
-# Let's look at the following:
-# * HyperParameter tuning
-# * Feature importance
-# * Confusion matrix
-# * Cross-validation
-# * Precision
-# * F1 score
-#
-# ## Hyperparameter Tuning
+# Hyperparameter Tuning
 
-# %% [markdown]
-# ### 1. KNN
-
-# %%
-# Let's tune KNN
+# Tuning KNN
 
 train_scores = []
 test_scores = []
 
-# Create a list of different values for n_neighbors
+# Tạo danh sách các giá trị khác nhau cho n_neighbors
 neighbors = range(1, 21)
 
-# Setup KNN instance
 knn = KNeighborsClassifier()
-
-# Loop thorugh different n_neighbors
 
 for i in neighbors:
     knn.set_params(n_neighbors=i)
-
-    # Fit the algorithm
+    
     knn.fit(X_train, y_train)
 
-    # Update the training scores list
     train_scores.append(knn.score(X_train, y_train))
 
-    # Update the testing scores list
+    # Đưa vào test score
     test_scores.append(knn.score(X_test, y_test))
 
-# %%
+
 knn.set_params(n_neighbors=np.argmax(test_scores) + 1)
 knn.fit(X_train, y_train)
 
-# %%
 plt.figure(figsize=(15, 8))
 plt.plot(neighbors, train_scores, label="Train score")
 plt.plot(neighbors, test_scores, label="Test score")
@@ -145,32 +113,19 @@ plt.ylabel("Model Scores")
 plt.legend()
 print(f"Maximum KNN score on the test data: {max(test_scores) * 100:.2f}%");
 
-# %% [markdown]
-# ## Hyperparameter tuning with Randomized SearchCV
-#
-# We're going to tune:
-#
-# 2. LogisticRegression()
-# 3. RandomForestCLassfier()
-#
-# ...Using RandomizedSearchCV
+# Hyperparameter tuning with Randomized SearchCV for LogisticRegression, RandomForestCLassfier
 
-# %%
-# Create a hyperparameter grid for Logistic Regression
+# Tạo hyperparameter grid cho Logistic Regression
 
 log_reg_grid = {"C": np.logspace(-4, 4, 30),
                 "solver": ["liblinear"]}
 
-# Create hyperparameter grid for RandomForestClassifier
+# Tạo hyperparameter grid cho RandomForestClassifier
 rf_grid = {"n_estimators": np.arange(10, 100, 50),
            "max_depth": [None, 3, 5, 10],
            "min_samples_split": np.arange(2, 20, 2),
            "min_samples_leaf": np.arange(1, 20, 2)}
 
-# %% [markdown]
-# Now We've got the hyperparameters grids setup for each of our models, let's tune them using RandomizedSearchCV...
-
-# %%
 # Tune LogisticRegression
 
 np.random.seed(42)
@@ -208,34 +163,17 @@ rs_rf = RandomizedSearchCV(RandomForestClassifier(),
 # Fit the random Hyperprameter search mode for randomforestclassifier()
 rs_rf.fit(X_train, y_train)
 
-# %%
 # Find the best hyperparameters
 print(rs_rf.best_params_)
 
-# %%
 # Evaluate the randomized search RandomForestClassifier model
 print(rs_rf.score(X_test, y_test))
 
-# %% [markdown]
-# ## Evaluating our tuned machine learning classifier, beyond accuracy.
-#
-# * Confusion Matrix
-# * Classification report
-# * Precision
-# * Recall
-# * F1-Score
-#
-# ...and it would be great if cross-validation was used where possible.
-#
-# To make comparisons and evaluate our trained model, first we need to make predictions.
-
-# %%
 sns.set(font_scale=1.5)
-
 
 def plot_conf_mat(y_test, y_preds):
     """
-    Plot a confusion matrix using seaborn's heatmap(
+    Dùng seaborn's heatmap để tạo ma trận nhầm lẫn(
     """
     fig, ax = plt.subplots(figsize=(3, 3))
     ax = sns.heatmap(confusion_matrix(y_test, y_preds),
@@ -245,13 +183,11 @@ def plot_conf_mat(y_test, y_preds):
     plt.ylabel("Predicted Label")
 
 
-# %%
 y_preds = knn.predict(X_test)
 plot_conf_mat(y_test, y_preds)
 print("KNN Classification Report:")
 print(classification_report(y_test, y_preds))
 
-# %%
 # Logistic Regression
 # Evaluate the model with best parameters
 rs_log_reg_model = rs_log_reg.best_estimator_
@@ -260,7 +196,6 @@ plot_conf_mat(y_test, y_preds_rs_log_reg_model)
 print("Logistic Regression Classification Report:")
 print(classification_report(y_test, y_preds_rs_log_reg_model))
 
-# %%
 # Random Forest
 # Evaluate the model with best parameters
 rs_rf_model = rs_rf.best_estimator_
@@ -269,36 +204,28 @@ plot_conf_mat(y_test, y_preds_rs_rf_model)
 print("Random Forest Classification Report for Random Forest:")
 print(classification_report(y_test, y_preds_rs_rf_model))
 
-# %% [markdown]
-# ### Calculate evaluation metrics using CrossValidation
-
-# %% [markdown]
-# Modelling our best model with its best parameter
-
-# %%
 knn.score(X_test, y_test)
 
-# %%
 # Cross-validated accuaracy
 cv_acc_knn = cross_val_score(knn, X, y, cv=5, scoring="accuracy")
 cv_acc_knn = np.mean(cv_acc_knn)
 
-# %%
+
 # Cross-validated precision
 cv_precision_knn = cross_val_score(knn, X, y, cv=5, scoring="precision")
 cv_precision_knn = np.mean(cv_precision_knn)
 
-# %%
+
 # Cross-validated recall
 cv_recall_knn = cross_val_score(knn, X, y, cv=5, scoring="recall")
 cv_recall_knn = np.mean(cv_recall_knn)
 
-# %%
+
 # Cross-validated f1-score
 cv_f1_knn = cross_val_score(knn, X, y, cv=5, scoring="f1")
 cv_f1_knn = np.mean(cv_f1_knn)
 
-# %%
+
 # Visuzalize cross-validated metrics
 cv_metrics = pd.DataFrame({"Accuracy": cv_acc_knn,
                            "Precision": cv_precision_knn,
@@ -309,7 +236,7 @@ cv_metrics.T.plot.bar(title="Cross-validated classification metrics for KNN",
                       legend=False)
 plt.xticks(rotation=0);
 
-# %%
+# LogististRegression
 # Cross-validated accuracy
 cv_acc_lr = cross_val_score(rs_log_reg_model, X, y, cv=5, scoring="accuracy")
 cv_acc_lr = np.mean(cv_acc_lr)
@@ -333,7 +260,7 @@ cv_metrics.T.plot.bar(title="Cross-validated classification metrics for LR",
 plt.xticks(rotation=0)
 plt.show()
 
-# %%
+# RandomForestRandomForest
 # Cross-validated accuracy
 cv_acc_rf = cross_val_score(rs_rf_model, X, y, cv=5, scoring="accuracy")
 cv_acc_rf = np.mean(cv_acc_rf)
@@ -357,7 +284,6 @@ cv_metrics.T.plot.bar(title="Cross-validated classification metrics for Random F
 plt.xticks(rotation=0)
 plt.show()
 
-# %%
 # Lưu kết quả của tất cả các mô hình vào DataFrame
 cv_results = pd.DataFrame({
     'Model': ['KNN', 'Logistic Regression', 'Random Forest'],
@@ -372,7 +298,6 @@ cv_results.set_index('Model').plot.bar(title="Comparison of models with cross-va
 plt.xticks(rotation=0)
 plt.show()
 
-# %%
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Giả sử y_test là nhãn thật và y_preds là các dự đoán của mô hình
@@ -388,46 +313,33 @@ mae_rf_cv = np.round(mean_absolute_error(y_test, y_preds_rs_rf_model), 2)
 mse_rf_cv = np.round(mean_squared_error(y_test, y_preds_rs_rf_model), 2)
 rmse_rf_cv = np.round(np.sqrt(mean_squared_error(y_test, y_preds_rs_rf_model)), 2)
 
-# %%
 models = ['KNeighborsClassifier', 'LogisticRegression', 'RandomForestClassifier']
 data = [[mae_knn_cv, mse_knn_cv, rmse_knn_cv], [mae_lr_cv, mse_lr_cv, rmse_lr_cv], [mae_rf_cv, mse_rf_cv, rmse_rf_cv]]
 cols = ['mae', 'mse', 'rmse']
 pd.DataFrame(data=data, index=models, columns=cols)
 
-# %% [markdown]
-# ### Feature Importance
-#
-# Feature Importance is another as asking, "which feature contributed" most to the outcomes of the model and how did they contributed<br>
-# Finding Feature imp. is different for each machine learning model<br>
-# Let's Find the feature importance for our LogisticRegression Model (Because knn do not have feature importance and it is our second best performing model)<br>
-
-# %%
-# Fit an instance of LogisticRegression
+# Feature Importance
 
 clf = LogisticRegression(C=0.20433597178569418,
                          solver='liblinear')
 clf.fit(X_train, y_train);
 
-# %%
 # Check Coef_
 print(clf.coef_)
 
-# %%
 # Match coef's of features to coloumns
 
 feature_dict = dict(zip(df.columns, list(clf.coef_[0])))
 print(feature_dict)
 
-# %%
 # Visualize Feature Importance
 feature_df = pd.DataFrame(feature_dict, index=[0])
 feature_df.T.plot.bar(title="Feature importance", legend=False);
 
-# %%
 pd.crosstab(df.gender, df.target)
 
-# %%
-# Save the model
+
+# Lưu mô hình có hiệu quả tốt nhất
 model_filename = 'E:\\Python Projects\\Heart-Disease_Project\\model\\model_ML_2.pkl'
 with open(model_filename, 'wb') as file:
     pickle.dump(rs_rf_model, file)
